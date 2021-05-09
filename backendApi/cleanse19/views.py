b76b5ddca9549f4166ce2282123381b5f19515cd
Sayali Moghe <sayali.moghe.1008@gmail.com>
2021-05-10 01:27:59 +0530

    CSS II

--- backendApi/cleanse19/views.py
+++ backendApi/cleanse19/views.py
@@ -252,6 +252,7 @@ def profile_save(request):
         u.username = username
         u.email=email
         u.save()
+        messages.info(request,'Saved changes!')
         return redirect('profile')
     # return render(request,'home.html')
 
@@ -270,6 +271,11 @@ def send_email(request):
         recipient_list = ['cleanse19.app@gmail.com']
         send_mail(subject, message, email_from, recipient_list)
         return render(request,'home.html')
+
+@login_required(login_url = '/')
+def analysisView(request):
+    user = user_data(request)
+    return render(request, 'analysis.html', user)
 
 @login_required(login_url= '/')
 def analysis(request):
@@ -311,8 +317,9 @@ def analysis(request):
                 if i.violations > people[i.timestamp.strftime('%x')][int(i.timestamp.strftime('%H'))]:
                     people[i.timestamp.strftime("%x")][int(i.timestamp.strftime('%H'))] = i.violations
         
-        print(face)
-        return render(request,'analysis.html', {'face': face, 'social': social, 'people': people})
+        # print(face)
+        # return render(request, 'analysis.html', {'face': face, 'social': social, 'people': people})
+        return JsonResponse({'face': face, 'social': social, 'people': people})
     return render(request,'analysis.html')
 
 @login_required(login_url = '/')
